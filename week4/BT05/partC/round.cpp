8dc1aa37520b0636dfee6588ce4bfc43bf7dfe03
Chu Huy Quang <quangdtm2004@gmail.com>
2023-02-17 08:34:14 +0700

    fix BT05

--- week4/BT05/partC/round.cpp
+++ week4/BT05/partC/round.cpp
@@ -1,13 +1,14 @@
 #include <cmath>
 
 int round__(double num) {
-    return (int)std::round(num);
+    return (int)round(num);
 }
 
 int round_(double num) {
     if (num >= 0) {
         return (int)(num + 0.5);
-    } else {
+    } 
+    else {
         return (int)(num - 0.5);
     }
 }
